fadfa913f9857a164e7eb39edafb5748cfa1e71c
Sharangowda s patil <sharangowda2425@gmail.com>
2017-01-05 14:03:58 +0530

    DAY 4

---
+++ First_Components.js
@@ -0,0 +1,110 @@
+/*var GreetingUniverse = React.createClass({
+        render: function() {
+          return (
+            <div>
+            <h1>Hello, Universe</h1>
+              <p>Hello, Universe</p>
+            </div>
+          )
+        }
+      });
+
+      var Greeting = React.createClass({
+        render: function() {
+          return (
+            <GreetingUniverse/>
+          )
+        }
+      }); */
+      //alert("?");
+    /*var app = "<h1>Hello world</h1>"
+    var mountComponent = document.querySelector('#app');
+    ReactDOM.render(app, mountComponent);*/
+
+
+// Like all other React components, this ES6 class will extend the 
+//React.Component class from the React package:
+    /*class App extends React.Component {
+  render() {
+    return <h1>Hello from our app</h1>
+  }
+}*/
+
+/*Another way we could write our App component is by using the React.createClass() function.
+
+var App = React.createClass({
+  render: function() {
+    return <h1>Hello from our app</h1>
+  }
+});*/
+
+/*class Panel extends React.Component {
+  render() {
+    return (
+      <div className="notificationsFrame">
+        <div className="panel">
+          <div className="header">
+            
+            <div className="menuIcon">
+              <div className="dashTop"></div>
+              <div className="dashBottom"></div>
+              <div className="circle"></div>
+            </div>
+
+            <span className="title">Timeline</span>
+
+            <input
+              type="text"
+              className="searchInput"
+              placeholder="Search ..." />
+
+            <div className="fa fa-search searchIcon"></div>
+          </div>
+          <div className="content">
+            <div className="line"></div>
+            <div className="item">
+
+              <div className="avatar">
+                <img src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
+                Doug
+              </div>
+
+              <span className="time">
+                An hour ago
+              </span>
+              <p>Ate lunch</p>
+            </div>
+
+            <div className="item">
+              <div className="avatar">
+                <img src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
+              </div>
+
+              <span className="time">10 am</span>
+              <p>Read Day two article</p>
+            </div>
+
+            <div className="item">
+              <div className="avatar">
+                <img src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
+              </div>
+
+              <span className="time">10 am</span>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+
+            <div className="item">
+              <div className="avatar">
+                <img src="http://www.croop.cl/UI/twitter/images/doug.jpg" />
+              </div>
+
+              <span className="time">2:21 pm</span>
+              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}*/
